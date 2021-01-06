908e63ca0a8b57cacefa04a44dc38cc8cb53a5fb
teng <teng.pongsakorn@gmail.com>
2021-01-06 09:32:11 +0700

    Finished Stage 1/6, Print coffee steps

---
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -0,0 +1,16 @@
+package machine;
+
+public class CoffeeMachine {
+
+    final static String steps = "\nStarting to make a coffee\n" +
+            "Grinding coffee beans\n" +
+            "Boiling water\n" +
+            "Mixing boiled water with crushed coffee beans\n" +
+            "Pouring coffee into the cup\n" +
+            "Pouring some milk into the cup\n" +
+            "Coffee is ready!";
+
+    public static void main(String[] args) {
+        System.out.println(steps);
+    }
+}
